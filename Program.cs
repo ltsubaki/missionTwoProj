d76379a8403cf0253f96306198d452ea41e71321
liliantsubaki <liliantsubaki@LILIANTSUBABDEE>
2024-01-17 23:52:47 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Threading;
+using System.Xml.Linq;
+using missionTwoProj;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //initialize variables
+        int numRolls = 0;
+        diceRoll dr = new diceRoll();
+
+        //ask the user how many times they want to roll the dice
+        System.Console.WriteLine("Please enter the number of times you want to roll two dice: ");
+        numRolls = int.Parse(System.Console.ReadLine());
+
+        //call the doDiceRoll class to execute the rolling
+        dr.doDiceRoll(numRolls);
+    }
+}
---
+++ diceRoll.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace missionTwoProj
+{
+    internal class diceRoll
+    {
+        public void doDiceRoll(int numRolls)
+        {
+            //initialize variables
+            int die1 = 0;
+            int die2 = 0;
+            int total = 0;
+
+            int[] rollTotals = new int[13];
+
+            double currPercent = 0.0;
+            string finalOutput = "";
+
+
+            //roll the two dice
+            //create a Random object
+            Random random = new Random();
+
+            //for the number of times that the user sepcified
+            for (int i = 0; i < numRolls; i++) 
+            {
+                //generate a random number between 1 and 6
+                die1 = random.Next(1, 7);
+                die2 = random.Next(1, 7);
+
+                total = die1 + die2;
+                rollTotals[total]++;
+            }
+
+            //for every possible total rolled
+            for (int i = 2; i < rollTotals.Length; i++)
+            {
+                string asteriskOutput = "";
+
+                //add to a string the number of asterisks (representing percent)
+                currPercent = (int)((double)(rollTotals[i]) / numRolls * 100);
+                asteriskOutput = new string('*', (int)currPercent);
+                finalOutput = finalOutput + i + ": " + asteriskOutput + "\n";
+            }
+        System.Console.WriteLine(finalOutput);
+        }
+    }
+}
